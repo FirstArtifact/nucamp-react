f8017d694e13c4a0bb69df4cd09527b3e19db33d
FirstArtifact <theartifactmedia@gmail.org>
2020-06-05 16:49:27 -0400

    [WEEK3] Changed version of functional component

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function RenderPartner({partner}) {
+const RenderPartner = ({partner}) => {
     if (partner) {
         return (
             <React.Fragment>
