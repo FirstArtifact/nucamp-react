13aa1bbbacb416d151b97f8eca6c32a1124710ef
Christopher Duncan <theartifactmedia@gmail.com>
2020-10-12 01:57:13 -0400

    [WORKSHOP] Updated Promotions file

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,24 +1,18 @@
-import * as ActionTypes from "./ActionTypes";
+import * as ActionTypes from './ActionTypes';
+export const Promotions = (state = { isLoading: true,
+                                        errMess: null,
+                                        promotions: [] }, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_PROMOTIONS:
+            return {...state, isLoading: false, errMess: null, promotions: action.payload};
 
-export const Partners = (
-  state = { isLoading: true, errMess: null, partners: [] },
-  action
-) => {
-  switch (action.type) {
-    case ActionTypes.ADD_PARTNERS:
-      return {
-        ...state,
-        isLoading: false,
-        errMess: null,
-        partners: action.payload,
-      };
+        case ActionTypes.PROMOTIONS_LOADING:
+            return {...state, isLoading: true, errMess: null, promotions: []}
 
-    case ActionTypes.PARTNERS_LOADING:
-      return { ...state, isLoading: true, errMess: null, partners: [] };
-
-    case ActionTypes.PARTNERS_FAILED:
-      return { ...state, isLoading: false, errMess: action.payload };
-    default:
-      return state;
-  }
+        case ActionTypes.PROMOTIONS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+            
+        default:
+          return state;
+      }
 };
