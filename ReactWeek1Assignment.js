7eba08f5f0f72eb1add13fa29da28720c0ca818d
Christopher Duncan <theartifactmedia@gmail.com>
2020-12-19 02:01:49 -0500

    [WEEK1] Added extra piece

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -16,7 +16,9 @@ class Bootcamp {
     registerStudent(student) {
         //if(this.students.some(s => s.email === student.email)) {
         if (this.students.filter(s => s.email === student.email).length) {
-            console.log(`The student with the email '${student.email}' is already registered to ${this.name} under the name "${student.name}"`);
+            const registeredStudent = this.students.filter(s => s.email === student.email)
+            console.log(registeredStudent);
+            console.log(`The student with the email '${student.email}' is already registered to ${this.name} under the name "${registeredStudent[0].name}"`);
         }
         else {
             this.students.push(student);
